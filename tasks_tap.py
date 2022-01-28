cec6eea2ad6e1bdc6404a4c260421c399e92cc19
andresporras3423 <oscarrussi@outlook.com>
2022-01-28 16:45:34 -0500

    add tap for tasks resource

---
+++ tasks_tap.py
@@ -0,0 +1,25 @@
+from jazzhr_tap import run_jazz_tap
+
+route = "tasks"
+schema = {'properties': {
+    'id': {'type': 'string'},
+    'object_id': {'type': 'string'},
+    'owner_id': {'type': 'string'},
+    'opener_name': {'type': 'string'},
+    'assignee_id': {'type': 'string'},
+    'assignee_name': {'type': 'string'},
+    'status': {'type': 'string'},
+    'description': {'type': 'string'},
+    'notes': {'type': 'string'},
+    'date_due': { "type": "string", "format": "date"},
+    'due_whenever': {'type': 'boolean'},
+    'date_created': { "type": "string", "format": "date"},
+    'time_created': { "type": "string", "format": "time"}
+    },
+    "primary_key": "id"
+  }
+stream = "jazzhr_tasks"
+def read_record(item):
+  item["due_whenever"] = True if item["due_whenever"]=='Yes' else False
+  return item
+run_jazz_tap(route, schema, stream, read_record)
