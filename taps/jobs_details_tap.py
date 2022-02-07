3a81aad0e9dd14c11d60019072e70e25e184019f
andresporras3423 <oscarrussi@outlook.com>
2022-02-07 17:03:24 -0500

    add tap for jobs details

--- taps/jobs_details_tap.py
+++ taps/jobs_details_tap.py
@@ -6,25 +6,25 @@ import requests
 from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '../.env')
-schema_path = join(dirname(__file__), '../schemas/users_details.json')
+schema_path = join(dirname(__file__), '../schemas/jobs_details.json')
 with open(schema_path, encoding='utf-8') as json_schema:
   schema = json.load(json_schema)
 load_dotenv(dotenv_path)
-stream="jazzhr_users_details"
+stream="jazzhr_jobs_details"
 
 JAZZHR_KEY = os.environ.get("jazzhr_key")
 endpoint = "https://api.resumatorapi.com/v1/"
 page = 1
 
 
-def retrieve_jazzhr_users():
-  authenticated_endpoint = f"{endpoint}users/page/{page}?apikey={JAZZHR_KEY}"
+def retrieve_jazzhr_jobs():
+  authenticated_endpoint = f"{endpoint}jobs/page/{page}?apikey={JAZZHR_KEY}"
   api_response = requests.get(authenticated_endpoint).json()
   return [r["id"] for r in api_response]
 
 
-def retrieve_jazzhr_users_details(index):
-  authenticated_endpoint = f"{endpoint}users/{index}?apikey={JAZZHR_KEY}"
+def retrieve_jazzhr_jobs_details(index):
+  authenticated_endpoint = f"{endpoint}jobs/{index}?apikey={JAZZHR_KEY}"
   api_response = requests.get(authenticated_endpoint).json()
   return api_response
 
@@ -33,17 +33,19 @@ singer.write_schema(
   stream_name=stream,
   schema=schema,
   key_properties=["id"])
-users = []
+jobs = []
 pursue = True
 while pursue:
-  response = retrieve_jazzhr_users()
-  users = users + response
+  response = retrieve_jazzhr_jobs()
+  jobs = jobs + response
   page = page + 1
   if len(response) < 100:
     pursue = False
-for user_ in users:
-  user_details = retrieve_jazzhr_users_details(user_)
-  if not isinstance(user_details["user_activity"], list):
-    user_details["user_activity"] = [user_details["user_activity"]]
+for job_ in jobs:
+  job_details = retrieve_jazzhr_jobs_details(job_)
+  if not isinstance(job_details["job_applicants"], list):
+    job_details["job_applicants"] = [job_details["job_applicants"]]
+  job_details["minimum_salary"] = float(job_details["minimum_salary"])
+  job_details["maximum_salary"] = float(job_details["maximum_salary"])
   singer.write_record(stream_name=stream,
-                    record=user_details)
+                    record=job_details)
