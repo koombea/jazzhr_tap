7d0532d154191cdbac0bd9f19dc8fed771296a12
andresporras3423 <oscarrussi@outlook.com>
2022-02-18 10:36:18 -0500

    changes after pylint fixing and autopep running, disable consider-using-with warning

--- jazzhr_tap/app.py
+++ jazzhr_tap/app.py
@@ -1,17 +1,18 @@
-from flask import Flask
 import subprocess
 import os
+from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
   subprocess.Popen(
     'python3 -m jazzhr_resources | target-stitch --config ./jazzhr_resources/config.json',
     stdin=subprocess.PIPE,
-    shell=True
-  )
+    shell=True)
   return "done"
+
 
 port = os.environ.get("PORT", 8080)
 app.run(debug=False, port=port, host='0.0.0.0')
